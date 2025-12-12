6e2be4468844ec2495c8f56fb7f4476d3066b520
johnmarkoush21-png <johnmarkoush21@gmail.com>
2025-12-12 10:53:41 +0800

    model area

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -10,29 +10,85 @@ class Report extends Model
     use HasFactory;
 
     protected $fillable = [
-        'user_id',
-        'name',
-        'email',
-        'phone',
-        'title',
-        'description',
-        'image',
-        'type',
-        'subtype',
-        'location',
-        'urgency',
-        'anonymous',
-        'status',
-        'evacuation_site',
-        'dispatch_unit',
-        'contact_person',
-        'contact_number',
-        'casualties',
-        'gender'
-    ];
+    'user_id',
+    'name',
+    'email',
+    'phone',
+    'title',
+    'description',
+    'image',
+    'video',
+    'type',
+    'subtype',
+    'location',
+    'urgency',
+    'anonymous',
+    'status',
+
+    // Existing resolution fields
+    'evacuation_site',    // already in DB before
+    'dispatch_unit',
+    'contact_person',
+    'contact_number',
+
+    // Demographics
+    'casualties',
+    'gender',
+
+    // NEW BDRRM FIELDS
+    'overseer',
+    'medical_response',
+    'ambulance_units',
+    'designated_hospitals',
+    'hospital_address',
+
+    'evacuation_address',
+    'evacuation_transport',
+    'evacuation_transport_unit',
+    'relief_goods_provider',
+
+    'pnp_station',
+    'pnp_patrol_unit',
+
+    'fire_department',
+    'fire_team',
+    'fire_truck_units',
+
+    'water_rescue_response_unit',
+    'rubber_boat_units',
+    'lifeguard_rescue_personnel',
+    'search_rescue_team',
+
+    'safety_and_security',
+    'relief_welfare',
+
+    'road_clearance_team',
+    'traffic_diversion_sites',
+    'first_aid_station',
+
+    'responding_team_complaints',
+    'complaints_actions',
+
+    'responding_team_suggestion',
+
+    'inspection_date',
+    'recommended_action',
+    'clearing_teams',
+    'power_utility_agency',
+    'structural_assessment_teams',
+    'pnp_team_unit',
+
+
+
+];
+
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
+    public function response()
+{
+    return $this->hasOne(Response::class, 'report_id');
+}
 }
---
+++ app/Models/Response.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Response extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'response_id';
+
+    protected $fillable = [
+        'report_id',
+        'reference_ID',
+        'response_datetime',
+        'dispatch_unit',
+        'contact_person',
+        'contact_number',
+        'overseer',
+        'medical_response',
+        'ambulance_units',
+        'designated_hospitals',
+        'hospital_address',
+        'hospital_latitude',
+        'hospital_longitude',
+        'evacuation_address',
+        'evacuation_latitude',
+        'evacuation_longitude',
+        'evacuation_transport',
+        'evacuation_transport_unit',
+        'relief_goods_provider',
+        'pnp_station',
+        'pnp_patrol_unit',
+        'pnp_team_unit',
+        'fire_department',
+        'fire_team',
+        'fire_truck_units',
+        'water_rescue_response_unit',
+        'lifeguard_rescue_personnel',
+        'search_rescue_team',
+        'safety_and_security',
+        'relief_welfare',
+        'road_clearance_team',
+        'traffic_diversion_sites',
+        'first_aid_station',
+        'responding_team_complaints',
+        'complaints_actions',
+        'responding_team_suggestion',
+        'inspection_date',
+        'recommended_action',
+        'clearing_teams',
+        'power_utility_agency',
+        'structural_assessment_teams',
+    ];
+
+    public function report()
+    {
+        return $this->belongsTo(Report::class, 'report_id');
+    }
+}
