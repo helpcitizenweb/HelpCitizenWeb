46f4b644160f99a04002f9ee7256ef25edfc2774
johnmarkoush21-png <johnmarkoush21@gmail.com>
2026-09-06 17:41:37 +0800

    response status update

--- app/Notifications/ReportStatusUpdatedNotification.php
+++ app/Notifications/ReportStatusUpdatedNotification.php
@@ -5,6 +5,8 @@ namespace App\Notifications;
 use App\Models\Report;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
+use NotificationChannels\WebPush\WebPushChannel;
+use NotificationChannels\WebPush\WebPushMessage;
 
 class ReportStatusUpdatedNotification extends Notification
 {
@@ -21,7 +23,10 @@ class ReportStatusUpdatedNotification extends Notification
 
     public function via($notifiable)
     {
-        return ['database'];
+        return [
+            'database',
+            WebPushChannel::class,
+        ];
     }
 
     public function toDatabase($notifiable)
@@ -31,5 +36,16 @@ class ReportStatusUpdatedNotification extends Notification
             'message'   => "Your report (ID #{$this->report->id}) status has been updated to {$this->newStatus}.",
             'url'       => route('reports.full', $this->report->id),
         ];
+    }
+
+    public function toWebPush($notifiable, $notification)
+    {
+        return (new WebPushMessage)
+            ->title('HelpCitizen Report Update')
+            ->body("Your report (ID #{$this->report->id}) status has been updated to {$this->newStatus}.")
+            ->icon('/favicon.ico')
+            ->data([
+                'url' => route('reports.full', $this->report->id),
+            ]);
     }
 }
